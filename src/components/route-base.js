bc8d8ae6c6e30313f175d1a2079d4ae77f800efc
Amine <amine@anava.tn>
2020-05-19 18:58:07 +0100

    first commit

---
+++ src/components/route-base.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RouteBase = () => {
+    return (
+        <>
+            <h1>This is a base router</h1>
+        </>
+    )
+}
+
+export default RouteBase;
---
+++ src/components/route-sercet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RouteSecret = () => {
+    return (
+        <>
+            <h1>This is a very secret stuff!</h1>
+        </>
+    )
+}
+
+export default RouteSecret;
